2c578b5bc193c8d9668fc234c52337424b184f3b
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2023-02-28 18:26:11 +0000

    use the modified version for openapi-generator

--- scripts/fetch-api.sh
+++ scripts/fetch-api.sh
@@ -9,7 +9,7 @@ endpoints=(
     "https://raw.githubusercontent.com/bf2fc6cc711aee1a0c2a/srs-fleet-manager/main/core/src/main/resources/srs-fleet-manager.json"
     "https://raw.githubusercontent.com/5733d9e2be6485d52ffa08870cabdee0/sandbox/main/openapi/smartevents_mgmt_v2.yaml"
     "https://raw.githubusercontent.com/bf2fc6cc711aee1a0c2a/kafka-admin-api/main/kafka-admin/.openapi/kafka-admin-rest.yaml"
-    "https://raw.githubusercontent.com/Apicurio/apicurio-registry/main/common/src/main/resources/META-INF/openapi.json"
+    "https://raw.githubusercontent.com/Apicurio/apicurio-registry/main/app/src/main/resources-unfiltered/META-INF/resources/api-specifications/registry/v2/openapi-gen.json"
     "https://sso.redhat.com/auth/realms/redhat-external/apis/openapi.yaml"
 )
 
@@ -19,7 +19,7 @@ for endpoint in ${endpoints[*]}; do
     if [[ ${filename} == "openapi.yaml" ]]; then
         filename="service-accounts.yaml"
     fi
-    if [[ ${filename} == "openapi.json" ]]; then
+    if [[ ${filename} == "openapi-gen.json" ]]; then
         filename="registry-instance.json"
     fi
 
